ca77a6081b89ea51247530c584291abe0cabc8ef
Ryan Greenblatt <guest287@node474.oscar.ccv.brown.edu>
2019-04-02 18:29:10 -0400

    added different log

--- hw/hw_3/run_all_benchmarks.sh
+++ hw/hw_3/run_all_benchmarks.sh
@@ -4,7 +4,7 @@ sbatch -J hw_3_16_1 -n 16  --nodes=1 -o 16_1.out  -e 16_1_error.out  run_benchma
 sbatch -J hw_3_32_1 -n 32  --nodes=2 -o 32_1.out  -e 32_1_error.out  run_benchmark.sh
 sbatch -J hw_3_64_1 -n 64  --nodes=4 -o 64_1.out  -e 64_1_error.out  run_benchmark.sh
 
-sbatch -J hw_3_1_1  -c 1   --nodes=1 -o 1_1.out   -e 1_1_error.out   run_benchmark.sh
+sbatch -J hw_3_1_2  -c 2   --nodes=1 -o 1_2.out   -e 1_2_error.out   run_benchmark.sh
 
 sbatch -J hw_3_1_8  -c 8   --nodes=1 -o 1_8.out   -e 1_8_error.out   run_benchmark.sh
 sbatch -J hw_3_1_16 -c 16  --nodes=1 -o 1_16.out  -e 1_16_error.out  run_benchmark.sh
