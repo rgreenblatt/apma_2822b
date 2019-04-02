186a0dc73c3f38e02ff54864092a3cbb4dba2b39
Ryan Greenblatt <guest287@node474.oscar.ccv.brown.edu>
2019-04-02 01:18:25 -0400

    updated logs

--- hw/hw_3/run_all_benchmarks.sh
+++ hw/hw_3/run_all_benchmarks.sh
@@ -2,7 +2,6 @@
 
 sbatch -J hw_3_16_1 -n 16  --nodes=1 -o 16_1.out  -e 16_1_error.out  run_benchmark.sh
 sbatch -J hw_3_32_1 -n 32  --nodes=2 -o 32_1.out  -e 32_1_error.out  run_benchmark.sh
-sbatch -J hw_3_48_1 -n 48  --nodes=3 -o 48_1.out  -e 48_1_error.out  run_benchmark.sh
 sbatch -J hw_3_64_1 -n 64  --nodes=4 -o 64_1.out  -e 64_1_error.out  run_benchmark.sh
 
 sbatch -J hw_3_1_8  -c 8   --nodes=1 -o 1_8.out   -e 1_8_error.out   run_benchmark.sh
