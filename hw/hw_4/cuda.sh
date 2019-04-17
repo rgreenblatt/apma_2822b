6a953bb3770608c34e16217ce8757a114e355abf
rgreenblatt <greenblattryan@gmail.com>
2019-04-17 13:27:28 -0400

    Column major is better

--- hw/hw_4/cuda.sh
+++ hw/hw_4/cuda.sh
@@ -3,8 +3,8 @@
 # Request a GPU partition node and access to 1 GPU
 #SBATCH -p gpu --gres=gpu:1
 
-# Request 1 CPU core
-#SBATCH -n 1
+# Request 8 CPU cores
+#SBATCH -n 8
 
 #SBATCH -t 00:05:00
 #SBATCH -o hw_04.out
