9164eb0d5b638bffd6bb87706a008b8a5dbee86d
rgreenblatt <greenblattryan@gmail.com>
2019-05-20 03:29:18 -0400

    Fixed run script

--- final/scripts/full_run.sh
+++ final/scripts/full_run.sh
@@ -1,74 +1,74 @@
 #!/bin/bash
 
-mpirun -np 1 ./bin/miniFE -nx 40
-mpirun -np 1 ./bin/miniFE -nx 80
-mpirun -np 1 ./bin/miniFE -nx 120
-mpirun -np 1 ./bin/miniFE -nx 160
-mpirun -np 1 ./bin/miniFE -nx 200
-mpirun -np 1 ./bin/miniFE -nx 240
+mpirun -np 1 ./bin/miniFE -nx 40  -ny 40  -nz 40 
+mpirun -np 1 ./bin/miniFE -nx 80 -ny 80 -nz 80
+mpirun -np 1 ./bin/miniFE -nx 120 -ny 120 -nz 120
+mpirun -np 1 ./bin/miniFE -nx 160 -ny 160 -nz 160
+mpirun -np 1 ./bin/miniFE -nx 200 -ny 200 -nz 200
+mpirun -np 1 ./bin/miniFE -nx 240 -ny 240 -nz 240
 
-mpirun -np 2 ./bin/miniFE -nx 40
-mpirun -np 2 ./bin/miniFE -nx 80
-mpirun -np 2 ./bin/miniFE -nx 120
-mpirun -np 2 ./bin/miniFE -nx 160
-mpirun -np 2 ./bin/miniFE -nx 200
-mpirun -np 2 ./bin/miniFE -nx 240
-mpirun -np 2 ./bin/miniFE -nx 280
-mpirun -np 2 ./bin/miniFE -nx 280
-mpirun -np 2 ./bin/miniFE -nx 320
+mpirun -np 2 ./bin/miniFE -nx 40 -ny 40 -nz 40
+mpirun -np 2 ./bin/miniFE -nx 80 -ny 80 -nz 80
+mpirun -np 2 ./bin/miniFE -nx 120 -ny 120 -nz 120
+mpirun -np 2 ./bin/miniFE -nx 160 -ny 160 -nz 160
+mpirun -np 2 ./bin/miniFE -nx 200 -ny 200 -nz 200
+mpirun -np 2 ./bin/miniFE -nx 240 -ny 240 -nz 240
+mpirun -np 2 ./bin/miniFE -nx 280 -ny 280 -nz 280
+mpirun -np 2 ./bin/miniFE -nx 280 -ny 280 -nz 280
+mpirun -np 2 ./bin/miniFE -nx 320 -ny 320 -nz 320
 
-mpirun -np 3 ./bin/miniFE -nx 40
-mpirun -np 3 ./bin/miniFE -nx 80
-mpirun -np 3 ./bin/miniFE -nx 120
-mpirun -np 3 ./bin/miniFE -nx 160
-mpirun -np 3 ./bin/miniFE -nx 200
-mpirun -np 3 ./bin/miniFE -nx 240
-mpirun -np 3 ./bin/miniFE -nx 280
-mpirun -np 3 ./bin/miniFE -nx 320
-mpirun -np 3 ./bin/miniFE -nx 360
-mpirun -np 3 ./bin/miniFE -nx 400
+mpirun -np 3 ./bin/miniFE -nx 40 -ny 40 -nz 40
+mpirun -np 3 ./bin/miniFE -nx 80 -ny 80 -nz 80
+mpirun -np 3 ./bin/miniFE -nx 120 -ny 120 -nz 120
+mpirun -np 3 ./bin/miniFE -nx 160 -ny 160 -nz 160
+mpirun -np 3 ./bin/miniFE -nx 200 -ny 200 -nz 200
+mpirun -np 3 ./bin/miniFE -nx 240 -ny 240 -nz 240
+mpirun -np 3 ./bin/miniFE -nx 280 -ny 280 -nz 280
+mpirun -np 3 ./bin/miniFE -nx 320 -ny 320 -nz 320
+mpirun -np 3 ./bin/miniFE -nx 360 -ny 360 -nz 360
+mpirun -np 3 ./bin/miniFE -nx 400 -ny 400 -nz 400
 
-mpirun -np 4 ./bin/miniFE -nx 40
-mpirun -np 4 ./bin/miniFE -nx 80
-mpirun -np 4 ./bin/miniFE -nx 120
-mpirun -np 4 ./bin/miniFE -nx 160
-mpirun -np 4 ./bin/miniFE -nx 200
-mpirun -np 4 ./bin/miniFE -nx 240
-mpirun -np 4 ./bin/miniFE -nx 280
-mpirun -np 4 ./bin/miniFE -nx 320
-mpirun -np 4 ./bin/miniFE -nx 360
-mpirun -np 4 ./bin/miniFE -nx 400
-mpirun -np 4 ./bin/miniFE -nx 440
-mpirun -np 4 ./bin/miniFE -nx 480
+mpirun -np 4 ./bin/miniFE -nx 40 -ny 40 -nz 40
+mpirun -np 4 ./bin/miniFE -nx 80 -ny 80 -nz 80
+mpirun -np 4 ./bin/miniFE -nx 120 -ny 120 -nz 120
+mpirun -np 4 ./bin/miniFE -nx 160 -ny 160 -nz 160
+mpirun -np 4 ./bin/miniFE -nx 200 -ny 200 -nz 200
+mpirun -np 4 ./bin/miniFE -nx 240 -ny 240 -nz 240
+mpirun -np 4 ./bin/miniFE -nx 280 -ny 280 -nz 280
+mpirun -np 4 ./bin/miniFE -nx 320 -ny 320 -nz 320
+mpirun -np 4 ./bin/miniFE -nx 360 -ny 360 -nz 360
+mpirun -np 4 ./bin/miniFE -nx 400 -ny 400 -nz 400
+mpirun -np 4 ./bin/miniFE -nx 440 -ny 440 -nz 440
+mpirun -np 4 ./bin/miniFE -nx 480 -ny 480 -nz 480
 
-mpirun -np 5 ./bin/miniFE -nx 40
-mpirun -np 5 ./bin/miniFE -nx 80
-mpirun -np 5 ./bin/miniFE -nx 120
-mpirun -np 5 ./bin/miniFE -nx 160
-mpirun -np 5 ./bin/miniFE -nx 200
-mpirun -np 5 ./bin/miniFE -nx 240
-mpirun -np 5 ./bin/miniFE -nx 280
-mpirun -np 5 ./bin/miniFE -nx 320
-mpirun -np 5 ./bin/miniFE -nx 360
-mpirun -np 5 ./bin/miniFE -nx 400
-mpirun -np 5 ./bin/miniFE -nx 440
-mpirun -np 5 ./bin/miniFE -nx 480
-mpirun -np 5 ./bin/miniFE -nx 520
-mpirun -np 5 ./bin/miniFE -nx 560
+mpirun -np 5 ./bin/miniFE -nx 40 -ny 40 -nz 40
+mpirun -np 5 ./bin/miniFE -nx 80 -ny 80 -nz 80
+mpirun -np 5 ./bin/miniFE -nx 120 -ny 120 -nz 120
+mpirun -np 5 ./bin/miniFE -nx 160 -ny 160 -nz 160
+mpirun -np 5 ./bin/miniFE -nx 200 -ny 200 -nz 200
+mpirun -np 5 ./bin/miniFE -nx 240 -ny 240 -nz 240
+mpirun -np 5 ./bin/miniFE -nx 280 -ny 280 -nz 280
+mpirun -np 5 ./bin/miniFE -nx 320 -ny 320 -nz 320
+mpirun -np 5 ./bin/miniFE -nx 360 -ny 360 -nz 360
+mpirun -np 5 ./bin/miniFE -nx 400 -ny 400 -nz 400
+mpirun -np 5 ./bin/miniFE -nx 440 -ny 440 -nz 440
+mpirun -np 5 ./bin/miniFE -nx 480 -ny 480 -nz 480
+mpirun -np 5 ./bin/miniFE -nx 520 -ny 520 -nz 520
+mpirun -np 5 ./bin/miniFE -nx 560 -ny 560 -nz 560
 
-mpirun -np 6 ./bin/miniFE -nx 40
-mpirun -np 6 ./bin/miniFE -nx 80
-mpirun -np 6 ./bin/miniFE -nx 120
-mpirun -np 6 ./bin/miniFE -nx 160
-mpirun -np 6 ./bin/miniFE -nx 200
-mpirun -np 6 ./bin/miniFE -nx 240
-mpirun -np 6 ./bin/miniFE -nx 280
-mpirun -np 6 ./bin/miniFE -nx 320
-mpirun -np 6 ./bin/miniFE -nx 360
-mpirun -np 6 ./bin/miniFE -nx 400
-mpirun -np 6 ./bin/miniFE -nx 440
-mpirun -np 6 ./bin/miniFE -nx 480
-mpirun -np 6 ./bin/miniFE -nx 520
-mpirun -np 6 ./bin/miniFE -nx 560
-mpirun -np 6 ./bin/miniFE -nx 600
-mpirun -np 6 ./bin/miniFE -nx 640
+mpirun -np 6 ./bin/miniFE -nx 40 -ny 40 -nz 40
+mpirun -np 6 ./bin/miniFE -nx 80 -ny 80 -nz 80
+mpirun -np 6 ./bin/miniFE -nx 120 -ny 120 -nz 120
+mpirun -np 6 ./bin/miniFE -nx 160 -ny 160 -nz 160
+mpirun -np 6 ./bin/miniFE -nx 200 -ny 200 -nz 200
+mpirun -np 6 ./bin/miniFE -nx 240 -ny 240 -nz 240
+mpirun -np 6 ./bin/miniFE -nx 280 -ny 280 -nz 280
+mpirun -np 6 ./bin/miniFE -nx 320 -ny 320 -nz 320
+mpirun -np 6 ./bin/miniFE -nx 360 -ny 360 -nz 360
+mpirun -np 6 ./bin/miniFE -nx 400 -ny 400 -nz 400
+mpirun -np 6 ./bin/miniFE -nx 440 -ny 440 -nz 440
+mpirun -np 6 ./bin/miniFE -nx 480 -ny 480 -nz 480
+mpirun -np 6 ./bin/miniFE -nx 520 -ny 520 -nz 520
+mpirun -np 6 ./bin/miniFE -nx 560 -ny 560 -nz 560
+mpirun -np 6 ./bin/miniFE -nx 600 -ny 600 -nz 600
+mpirun -np 6 ./bin/miniFE -nx 640 -ny 640 -nz 640
