0abf661afedfb4916c4f36716dece5d043187eac
rgreenblatt <greenblattryan@gmail.com>
2019-05-19 21:05:00 -0400

    Added script for doing a full set of tests

---
+++ final/scripts/full_run.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+mpirun -np 1 ./bin/miniFE -nx 40
+mpirun -np 1 ./bin/miniFE -nx 80
+mpirun -np 1 ./bin/miniFE -nx 120
+mpirun -np 1 ./bin/miniFE -nx 160
+mpirun -np 1 ./bin/miniFE -nx 200
+mpirun -np 1 ./bin/miniFE -nx 240
+
+mpirun -np 2 ./bin/miniFE -nx 40
+mpirun -np 2 ./bin/miniFE -nx 80
+mpirun -np 2 ./bin/miniFE -nx 120
+mpirun -np 2 ./bin/miniFE -nx 160
+mpirun -np 2 ./bin/miniFE -nx 200
+mpirun -np 2 ./bin/miniFE -nx 240
+mpirun -np 2 ./bin/miniFE -nx 280
+mpirun -np 2 ./bin/miniFE -nx 280
+mpirun -np 2 ./bin/miniFE -nx 320
+
+mpirun -np 3 ./bin/miniFE -nx 40
+mpirun -np 3 ./bin/miniFE -nx 80
+mpirun -np 3 ./bin/miniFE -nx 120
+mpirun -np 3 ./bin/miniFE -nx 160
+mpirun -np 3 ./bin/miniFE -nx 200
+mpirun -np 3 ./bin/miniFE -nx 240
+mpirun -np 3 ./bin/miniFE -nx 280
+mpirun -np 3 ./bin/miniFE -nx 320
+mpirun -np 3 ./bin/miniFE -nx 360
+mpirun -np 3 ./bin/miniFE -nx 400
+
+mpirun -np 4 ./bin/miniFE -nx 40
+mpirun -np 4 ./bin/miniFE -nx 80
+mpirun -np 4 ./bin/miniFE -nx 120
+mpirun -np 4 ./bin/miniFE -nx 160
+mpirun -np 4 ./bin/miniFE -nx 200
+mpirun -np 4 ./bin/miniFE -nx 240
+mpirun -np 4 ./bin/miniFE -nx 280
+mpirun -np 4 ./bin/miniFE -nx 320
+mpirun -np 4 ./bin/miniFE -nx 360
+mpirun -np 4 ./bin/miniFE -nx 400
+mpirun -np 4 ./bin/miniFE -nx 440
+mpirun -np 4 ./bin/miniFE -nx 480
+
+mpirun -np 5 ./bin/miniFE -nx 40
+mpirun -np 5 ./bin/miniFE -nx 80
+mpirun -np 5 ./bin/miniFE -nx 120
+mpirun -np 5 ./bin/miniFE -nx 160
+mpirun -np 5 ./bin/miniFE -nx 200
+mpirun -np 5 ./bin/miniFE -nx 240
+mpirun -np 5 ./bin/miniFE -nx 280
+mpirun -np 5 ./bin/miniFE -nx 320
+mpirun -np 5 ./bin/miniFE -nx 360
+mpirun -np 5 ./bin/miniFE -nx 400
+mpirun -np 5 ./bin/miniFE -nx 440
+mpirun -np 5 ./bin/miniFE -nx 480
+mpirun -np 5 ./bin/miniFE -nx 520
+mpirun -np 5 ./bin/miniFE -nx 560
+
+mpirun -np 6 ./bin/miniFE -nx 40
+mpirun -np 6 ./bin/miniFE -nx 80
+mpirun -np 6 ./bin/miniFE -nx 120
+mpirun -np 6 ./bin/miniFE -nx 160
+mpirun -np 6 ./bin/miniFE -nx 200
+mpirun -np 6 ./bin/miniFE -nx 240
+mpirun -np 6 ./bin/miniFE -nx 280
+mpirun -np 6 ./bin/miniFE -nx 320
+mpirun -np 6 ./bin/miniFE -nx 360
+mpirun -np 6 ./bin/miniFE -nx 400
+mpirun -np 6 ./bin/miniFE -nx 440
+mpirun -np 6 ./bin/miniFE -nx 480
+mpirun -np 6 ./bin/miniFE -nx 520
+mpirun -np 6 ./bin/miniFE -nx 560
+mpirun -np 6 ./bin/miniFE -nx 600
+mpirun -np 6 ./bin/miniFE -nx 640
