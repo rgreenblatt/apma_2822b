f7d8a5589033f16c7ab1015b223e9b7a49f20ae7
rgreenblatt <greenblattryan@gmail.com>
2019-05-20 03:30:30 -0400

    Fixed over 4

--- final/scripts/full_run.sh
+++ final/scripts/full_run.sh
@@ -40,35 +40,3 @@ mpirun -np 4 ./bin/miniFE -nx 360 -ny 360 -nz 360
 mpirun -np 4 ./bin/miniFE -nx 400 -ny 400 -nz 400
 mpirun -np 4 ./bin/miniFE -nx 440 -ny 440 -nz 440
 mpirun -np 4 ./bin/miniFE -nx 480 -ny 480 -nz 480
-
-mpirun -np 5 ./bin/miniFE -nx 40 -ny 40 -nz 40
-mpirun -np 5 ./bin/miniFE -nx 80 -ny 80 -nz 80
-mpirun -np 5 ./bin/miniFE -nx 120 -ny 120 -nz 120
-mpirun -np 5 ./bin/miniFE -nx 160 -ny 160 -nz 160
-mpirun -np 5 ./bin/miniFE -nx 200 -ny 200 -nz 200
-mpirun -np 5 ./bin/miniFE -nx 240 -ny 240 -nz 240
-mpirun -np 5 ./bin/miniFE -nx 280 -ny 280 -nz 280
-mpirun -np 5 ./bin/miniFE -nx 320 -ny 320 -nz 320
-mpirun -np 5 ./bin/miniFE -nx 360 -ny 360 -nz 360
-mpirun -np 5 ./bin/miniFE -nx 400 -ny 400 -nz 400
-mpirun -np 5 ./bin/miniFE -nx 440 -ny 440 -nz 440
-mpirun -np 5 ./bin/miniFE -nx 480 -ny 480 -nz 480
-mpirun -np 5 ./bin/miniFE -nx 520 -ny 520 -nz 520
-mpirun -np 5 ./bin/miniFE -nx 560 -ny 560 -nz 560
-
-mpirun -np 6 ./bin/miniFE -nx 40 -ny 40 -nz 40
-mpirun -np 6 ./bin/miniFE -nx 80 -ny 80 -nz 80
-mpirun -np 6 ./bin/miniFE -nx 120 -ny 120 -nz 120
-mpirun -np 6 ./bin/miniFE -nx 160 -ny 160 -nz 160
-mpirun -np 6 ./bin/miniFE -nx 200 -ny 200 -nz 200
-mpirun -np 6 ./bin/miniFE -nx 240 -ny 240 -nz 240
-mpirun -np 6 ./bin/miniFE -nx 280 -ny 280 -nz 280
-mpirun -np 6 ./bin/miniFE -nx 320 -ny 320 -nz 320
-mpirun -np 6 ./bin/miniFE -nx 360 -ny 360 -nz 360
-mpirun -np 6 ./bin/miniFE -nx 400 -ny 400 -nz 400
-mpirun -np 6 ./bin/miniFE -nx 440 -ny 440 -nz 440
-mpirun -np 6 ./bin/miniFE -nx 480 -ny 480 -nz 480
-mpirun -np 6 ./bin/miniFE -nx 520 -ny 520 -nz 520
-mpirun -np 6 ./bin/miniFE -nx 560 -ny 560 -nz 560
-mpirun -np 6 ./bin/miniFE -nx 600 -ny 600 -nz 600
-mpirun -np 6 ./bin/miniFE -nx 640 -ny 640 -nz 640
